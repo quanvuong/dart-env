ffefb82a048c58bbe1e6949ad5cd171ac86dcf8d
Wenhao Yu <stacormed@gmail.com>
2017-06-13 16:04:31 -0400

    update walker3d restricted

--- gym/envs/dart/hopper_cont.py
+++ gym/envs/dart/hopper_cont.py
@@ -21,7 +21,7 @@ class DartHopperEnvCont(dart_env.DartEnv, utils.EzPickle):
         obs_dim = 11
         self.param_manager = hopperContactMassManager(self)
         modelpath = os.path.join(os.path.dirname(__file__), "models")
-        upselector = joblib.load(os.path.join(modelpath, 'UPSelector_torsostrength_sd5_3class.pkl'))
+        upselector = joblib.load(os.path.join(modelpath, 'UPSelector_restfoot_sd6_loc_blurry.pkl'))
         self.sampling_selector = upselector
 
         if self.train_UP:
