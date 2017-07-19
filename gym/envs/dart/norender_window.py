12423e1ba4fbff49d199a4d8d21060973718a7ab
Alexander Clegg <aclegg3@gatech.edu>
2017-07-19 14:02:54 -0400

    modified:   gym/envs/dart/dart_env.py 	new file:   gym/envs/dart/norender_window.py 	modified:   gym/envs/dart/posereacher_cloth.py

---
+++ gym/envs/dart/norender_window.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class NoRenderWindow(object):
+    #This class replaces static_window.py: StaticGLUTWindow in order to bypass all openGL
+
+    def __init__(self, sim, title):
+        self.title = title
+        self.sim = sim
+        self.is_simulating = True
+
+    def runSingleStep(self):
+        if self.sim is None:
+            return
+        if self.is_simulating:
+            self.sim.step()
+
+    def run(self, _width=None, _height=None, _show_window=False):
+        #initialization should happen here
+        print("NoRenderWindow initialized")
--- gym/envs/dart/posereacher_cloth.py
+++ gym/envs/dart/posereacher_cloth.py
@@ -93,7 +93,7 @@ class DartClothPoseReacherEnv(DartClothEnv, utils.EzPickle):
         if self.usePoseTarget is True:
             observation_size += 22
 
-        DartClothEnv.__init__(self, cloth_scene=clothScene, model_paths='UpperBodyCapsules.skel', frame_skip=4, observation_size=(66+66+6), action_bounds=self.control_bounds)
+        DartClothEnv.__init__(self, cloth_scene=clothScene, model_paths='UpperBodyCapsules.skel', frame_skip=4, observation_size=(66+66+6), action_bounds=self.control_bounds, disableViewer=True, visualize=False)
         #DartClothEnv.__init__(self, cloth_scene=clothScene, model_paths='UpperBodyCapsules.skel', frame_skip=4, observation_size=(66+66+6), action_bounds=self.control_bounds, visualize=False)
 
         self.robot_skeleton.set_self_collision_check(True)
