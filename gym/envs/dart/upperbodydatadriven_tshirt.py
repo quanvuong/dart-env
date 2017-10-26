167478d7ad36562cd4978ad59ebf5a410ebeede1
Alexander Clegg <aclegg3@gatech.edu>
2017-10-26 16:49:56 -0400

    modified:   gym/envs/dart/upperbodydatadriven_tshirt.py

--- gym/envs/dart/upperbodydatadriven_tshirt.py
+++ gym/envs/dart/upperbodydatadriven_tshirt.py
@@ -34,7 +34,7 @@ class DartClothUpperBodyDataDrivenTshirtEnv(DartClothEnv, utils.EzPickle):
         self.ROMPointMinDistance = 1.0
 
         #sim variables
-        self.gravity = False
+        self.gravity = True
         self.resetRandomPose = True
         self.resetFile = self.prefix + "/assets/ROMPoints_upperbodycapsules_datadriven"
         self.dataDrivenJointLimts = True
