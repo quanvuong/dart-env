f840e9eb0450d2df2ef6cffa86f861e24e377d76
Alexander Clegg <aclegg3@gatech.edu>
2017-12-08 08:52:18 -0500

    modified:   gym/envs/dart/upperbodydatadriven_cloth_reacher.py

--- gym/envs/dart/upperbodydatadriven_cloth_reacher.py
+++ gym/envs/dart/upperbodydatadriven_cloth_reacher.py
@@ -22,7 +22,7 @@ import OpenGL.GLUT as GLUT
 class DartClothUpperBodyDataDrivenClothReacherEnv(DartClothUpperBodyDataDrivenClothBaseEnv, utils.EzPickle):
     def __init__(self):
         #feature flags
-        rendering = True
+        rendering = False
         clothSimulation = False
         renderCloth = False
 
