bb93c507240c4661cc8c59cdac0cd2b396fdf7c9
Alexander Clegg <aclegg3@gatech.edu>
2018-05-06 11:31:11 -0400

    modified:   gym/envs/dart/upperbodydatadriven_cloth_tshirtL.py

--- gym/envs/dart/upperbodydatadriven_cloth_tshirtL.py
+++ gym/envs/dart/upperbodydatadriven_cloth_tshirtL.py
@@ -51,7 +51,7 @@ class DartClothUpperBodyDataDrivenClothTshirtLEnv(DartClothUpperBodyDataDrivenCl
         self.limbProgressRewardWeight = 15
         self.oracleDisplacementRewardWeight = 50
         self.contactGeoRewardWeight = 4
-        self.deformationPenaltyWeight = 5  # was 5... then 1...
+        self.deformationPenaltyWeight = 1  # was 5... then 1...
         self.sleeveForwardRewardWeight = 3
         self.restPoseRewardWeight = 1
 
