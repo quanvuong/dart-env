2114382285eef4583b01abb61348887dc7fbbf1f
Alexander Clegg <aclegg3@gatech.edu>
2018-05-17 15:22:45 -0400

    modified:   gym/envs/dart/fullbodydatadriven_cloth_onefootstand_shorts3.py

--- gym/envs/dart/fullbodydatadriven_cloth_onefootstand_shorts3.py
+++ gym/envs/dart/fullbodydatadriven_cloth_onefootstand_shorts3.py
@@ -62,7 +62,7 @@ class DartClothFullBodyDataDrivenClothOneFootStandShorts3Env(DartClothFullBodyDa
         self.contactRewardWeight                = 1
         self.flatFootRewardWeight               = 4
         self.COMHeightRewardWeight              = 2
-        self.aliveBonusRewardWeight             = 15
+        self.aliveBonusRewardWeight             = 17
         self.stationaryAnkleAngleRewardWeight   = 0.025
         self.stationaryAnklePosRewardWeight     = 2
         self.footPosRewardWeight                = 4
