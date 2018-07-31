b316308126e12f3df6034ccfa324c896e2696102
Wenhao Yu <wenhaoyu@Wenhaos-MacBook-Pro.local>
2018-07-31 12:18:11 -0400

    update human balance

--- gym/envs/dart/human_balance.py
+++ gym/envs/dart/human_balance.py
@@ -3,21 +3,14 @@ __author__ = 'yuwenhao'
 import numpy as np
 from gym import utils
 from gym.envs.dart import dart_env
-import joblib
-import os
-
-import time
-
-import pydart2 as pydart
-
 
 class DartHumanBalanceEnv(dart_env.DartEnv, utils.EzPickle):
     def __init__(self):
         self.control_bounds = np.array([[1.0] * 23, [-1.0] * 23])
-        self.action_scale = 200#np.array([60.0, 200, 60, 100, 80, 60, 60, 200, 60, 100, 80, 60, 150, 150, 100, 15,80,15, 30, 15,80,15, 30])
+        self.action_scale = 200
         obs_dim = 57
 
-        dart_env.DartEnv.__init__(self, 'kima/kima_human_edited.skel', 15, obs_dim, self.control_bounds,
+        dart_env.DartEnv.__init__(self, 'kima/kima_human_balance.skel', 15, obs_dim, self.control_bounds,
                                       disableViewer=True, dt=0.002)
 
         self.push_direction = np.array([1, 0, 0])
@@ -25,24 +18,6 @@ class DartHumanBalanceEnv(dart_env.DartEnv, utils.EzPickle):
         self.push_target = 'thorax'
 
         self.robot_skeleton.set_self_collision_check(True)
-
-        for i in range(0, len(self.dart_world.skeletons[0].bodynodes)):
-            self.dart_world.skeletons[0].bodynodes[i].set_friction_coeff(1)
-        for i in range(0, len(self.dart_world.skeletons[1].bodynodes)):
-            self.dart_world.skeletons[1].bodynodes[i].set_friction_coeff(1)
-
-        # self.dart_world.set_collision_detector(3)
-
-        for bn in self.robot_skeleton.bodynodes:
-            if len(bn.shapenodes) > 0:
-                if hasattr(bn.shapenodes[0].shape, 'size'):
-                    shapesize = bn.shapenodes[0].shape.size()
-                    print('density of ', bn.name, ' is ', bn.mass()/np.prod(shapesize))
-                if hasattr(bn.shapenodes[0].shape, 'radius') and hasattr(bn.shapenodes[0].shape, 'height'):
-                    radius = bn.shapenodes[0].shape.radius()
-                    height = bn.shapenodes[0].shape.height()
-                    print('density of ', bn.name, ' is ', bn.mass()/(height * np.pi * radius ** 2))
-        print('Total mass: ', self.robot_skeleton.mass())
 
         utils.EzPickle.__init__(self)
 
@@ -66,14 +41,30 @@ class DartHumanBalanceEnv(dart_env.DartEnv, utils.EzPickle):
 
         alive_bonus = 3.0
 
-        reward = alive_bonus - np.square(a).sum() * 0.1
+        head_position = self.robot_skeleton.bodynode('head').C
+        hand_left_position = self.robot_skeleton.bodynode('l-lowerarm').C
+        hand_right_position = self.robot_skeleton.bodynode('r-lowerarm').C
+        foot_left_position = self.robot_skeleton.bodynode('l-foot').C
+        foot_right_position = self.robot_skeleton.bodynode('r-foot').C
+        pose_dev = np.exp(-np.square(np.clip(head_position - self.init_head_position,-10, 10)).sum())
+        pose_dev += np.exp(-np.square(np.clip(hand_left_position - self.init_hand_left_position,-10, 10)).sum())
+        pose_dev += np.exp(-np.square(np.clip(hand_right_position - self.init_hand_right_position,-10, 10)).sum())
+        pose_dev += np.exp(-np.square(np.clip(foot_left_position - self.init_foot_left_position,-10, 10)).sum())
+        pose_dev += np.exp(-np.square(np.clip(foot_right_position - self.init_foot_right_position,-10, 10)).sum())
+
+        current_q = self.robot_skeleton.q[6:]
+        q_dev = np.exp(-np.square(np.clip(self.init_q - current_q,-10, 10)).sum())
+
+        reward = alive_bonus - np.square(a).sum() * 0.1 + pose_dev + q_dev
 
         self.cur_step += 1
 
         s = self.state_vector()
 
+        if not (np.abs(s) < 100).all():
+            reward = 0.0
 
-        done = not (np.isfinite(s).all() and (np.abs(s[2:]) < 100).all() and
+        done = not (np.isfinite(s).all() and (np.abs(s) < 100).all() and
                     (height - self.init_height > -0.35) and
                     np.abs(self.robot_skeleton.q[5]) < 0.7 and np.abs(self.robot_skeleton.q[4]) < 0.7 and
                     np.abs(self.robot_skeleton.q[3]) < 0.7)
@@ -92,22 +83,25 @@ class DartHumanBalanceEnv(dart_env.DartEnv, utils.EzPickle):
 
     def reset_model(self):
         self.dart_world.reset()
-        self.total_work = 0
 
-        init_q = self.robot_skeleton.q
-        init_dq = self.robot_skeleton.dq
-
-        qpos = init_q + self.np_random.uniform(low=-.005, high=.005, size=self.robot_skeleton.ndofs)
-        qvel = init_dq + self.np_random.uniform(low=-.05, high=.05, size=self.robot_skeleton.ndofs)
+        qpos = self.robot_skeleton.q + self.np_random.uniform(low=-.005, high=.005, size=self.robot_skeleton.ndofs)
+        qvel = self.robot_skeleton.dq + self.np_random.uniform(low=-.05, high=.05, size=self.robot_skeleton.ndofs)
 
         self.set_state(qpos, qvel)
         self.cur_step = 0
 
         self.init_height = self.robot_skeleton.bodynode('head').com()[1]
 
+        self.init_head_position = self.robot_skeleton.bodynode('head').C
+        self.init_hand_left_position = self.robot_skeleton.bodynode('l-lowerarm').C
+        self.init_hand_right_position = self.robot_skeleton.bodynode('r-lowerarm').C
+        self.init_foot_left_position = self.robot_skeleton.bodynode('l-foot').C
+        self.init_foot_right_position = self.robot_skeleton.bodynode('r-foot').C
+        self.init_q = self.robot_skeleton.q[6:]
+
         self.push_direction = np.random.uniform(-1, 1, 3)
         self.push_direction[1] = 0
-        self.push_direction[0] = np.abs(self.push_direction[0])
+        #self.push_direction[0] = np.abs(self.push_direction[0])
         self.push_direction /= np.linalg.norm(self.push_direction)
         self.push_strength = (np.random.random() + 1.0) * 100
 
