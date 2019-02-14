0ce14102bc6f768d1dabad7ce013e3848539e797
Alexander Clegg <aclegg3@gatech.edu>
2019-02-13 20:10:57 -0500

    modified:   gym/envs/dart/upperbodydatadriven_cloth_iiwa_gown_multibot.py

--- gym/envs/dart/upperbodydatadriven_cloth_iiwa_gown_multibot.py
+++ gym/envs/dart/upperbodydatadriven_cloth_iiwa_gown_multibot.py
@@ -356,7 +356,7 @@ class DartClothUpperBodyDataDrivenClothIiwaGownMultibotEnv(DartClothEnv, utils.E
         self.hoopNormalObs  = False #if true, obs includes the normal vector of the hoop
         self.jointLimVarObs = False #if true, constraints are varied in reset and given as NN input
         self.actionScaleVarObs = False #if true, action scales are varied in reset and given as NN input
-        self.weaknessScaleVarObs = False #if true, scale torque limits on one whole side with a single value to model unilateral weakness
+        self.weaknessScaleVarObs = True #if true, scale torque limits on one whole side with a single value to model unilateral weakness
         self.elbowConVarObs = False  # if true, modify limits of the elbow joint
         self.SPDTargetObs = True  # need this to control this
 
@@ -1032,7 +1032,7 @@ class DartClothUpperBodyDataDrivenClothIiwaGownMultibotEnv(DartClothEnv, utils.E
         #compute gravity compenstation and set action scale for the state
         if self.weaknessScaleVarObs:
             if self.simpleWeakness:
-                for i in range(11,19):
+                for i in range(3,19):
                     #self.action_scale[i] = self.weaknessScale * self.initialActionScale[i]
                     self.human_action_scale[i] = LERP(self.action_scale_range[0][i], self.action_scale_range[1][i], self.weaknessScale)
             else:
